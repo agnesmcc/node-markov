8ed81d53596989734814df085da56f7cd83dc507
Agnes <agnes.mccall5@gmail.com>
2024-07-04 21:57:04 -0400

    complete markov machine

--- markov.js
+++ markov.js
@@ -8,6 +8,7 @@ class MarkovMachine {
   constructor(text) {
     let words = text.split(/[ \r\n]+/);
     this.words = words.filter(c => c !== "");
+    this.chains = {}
     this.makeChains();
   }
 
@@ -17,13 +18,52 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // TODO
+    for (let i = 0; i < this.words.length - 1; i++) {
+      const word = this.words[i];
+      const nextWord = this.words[i + 1];
+  
+      if (!this.chains[word]) {
+        this.chains[word] = [];
+      }
+  
+      this.chains[word].push(nextWord);
+    }
+  
+    const lastWord = this.words[this.words.length - 1];
+    if (!this.chains[lastWord]) {
+      this.chains[lastWord] = [null];
+    }
   }
-
 
   /** return random text from chains */
 
   makeText(numWords = 100) {
-    // TODO
+  let text = [];
+
+  const randomIndex = Math.floor(Math.random() * this.words.length);
+  let currentWord = this.words[randomIndex];
+  text.push(currentWord);
+
+  while (text.length < numWords) {
+    const nextWords = this.chains[currentWord];
+
+    if (nextWords.length === 0) {
+      break;
+    }
+
+    const randomIndex = Math.floor(Math.random() * nextWords.length);
+    const nextWord = nextWords[randomIndex];
+
+    if (nextWord === null) {
+      break;
+    }
+
+    text.push(nextWord);
+    currentWord = nextWord;
   }
+
+  return text.join(' ');
 }
+}
+
+module.exports = { MarkovMachine };
