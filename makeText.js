1a052b28984928b3bdaa7638f830e0240674853a
Agnes <agnes.mccall5@gmail.com>
2024-07-04 22:38:22 -0400

    add makeText.js

--- makeText.js
+++ makeText.js
@@ -1,2 +1,32 @@
 /** Command-line tool to generate Markov text. */
 
+const fs = require('fs');
+const axios = require('axios');
+const markov = require('./markov');
+
+function makeTextFromFile(filename) {
+    try {
+        const mm = new markov.MarkovMachine(fs.readFileSync(filename, 'utf8'));
+        console.log(mm.makeText());
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function makeTextFromURL(url) {
+    try {
+        const response = await axios.get(url);
+        const mm = new markov.MarkovMachine(response.data);
+        console.log(await mm.makeText());
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const filenameOrURL = process.argv[3];
+
+if (process.argv[2] === 'url') {
+    makeTextFromURL(filenameOrURL);
+} else {
+    makeTextFromFile(filenameOrURL);
+}
